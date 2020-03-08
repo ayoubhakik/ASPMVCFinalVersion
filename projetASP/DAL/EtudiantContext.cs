8d5db57f87a6a83408e8c0778b1793ba2c9adf07
Sifamo <sifane.mouad@gmail.com>
2020-03-08 01:13:32 +0100

    Ajout des models: Etudiant et Filiere. Ajout du data context EtudiantContext sous le dossier DAL. Ajout du connection string dans Web.config

---
+++ projetASP/DAL/EtudiantContext.cs
@@ -0,0 +1,19 @@
+﻿using projetASP.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace projetASP.DAL
+{
+    public class EtudiantContext : DbContext
+    {
+        public EtudiantContext() : base("EtudiantContext")
+        {
+
+        }
+        public DbSet<Etudiant> etudiants { get; set; }
+        
+    }
+}
