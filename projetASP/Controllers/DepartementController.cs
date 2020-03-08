339c0fa0473b73daac90d8f4f6cf9839a4b571d4
ayoubhakik <ayoubhakikhakik@gmail.com>
2020-03-08 03:54:58 +0100

    controller de la classe departement

---
+++ projetASP/Controllers/DepartementController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace projetASP.Controllers
+{
+    public class DepartementController : Controller
+    {
+        // GET: Departement
+        public ActionResult Home()
+        {
+            return View();
+        }
+        
+        public ActionResult ImporterEtudiants()
+        {
+            return View();
+        }
+        public ActionResult ImporterNotes()
+        {
+            return View();
+        }
+        public ActionResult AttributionFiliere()
+        {
+            return View();
+        }
+        public ActionResult Statistiques()
+        {
+            return View();
+        }
+        public ActionResult Visualiser()
+        {
+            return View();
+        }
+    }
+}
