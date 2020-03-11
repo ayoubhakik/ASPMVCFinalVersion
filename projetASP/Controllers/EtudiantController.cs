f6334232c2069b882ee7d6de36e00d939d656fb0
kamallabdo <Abderahmane.kamal@gmail.com>
2020-03-11 16:30:16 +0100

    Modification de donnee de l Etudiant

--- projetASP/Controllers/EtudiantController.cs
+++ projetASP/Controllers/EtudiantController.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using projetASP.DAL;
+using projetASP.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,18 +14,93 @@ namespace projetASP.Controllers
     public class EtudiantController : Controller
     {
         // GET: Etudiant
+        EtudiantContext etudiantContext = new EtudiantContext();
         public ActionResult Index()
         {
             ViewBag.Current = "Home";
+         
             return View();
         }
 
+
+
+
+
+        //--------------------------------------------------------------------------------------------------------------------------
+        //Modification 
         public ActionResult Modification()
         {
             ViewBag.Current = "Modification";
-            return View();
+            ViewBag.check = "Checked";
+            /* if (id == null)
+             {
+                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+             }*/
+            Etudiant etudiants = etudiantContext.etudiants.Find(4);
+
+            if (etudiants == null)
+            {
+                return HttpNotFound();
+            }
+           
+            return View(etudiants);
         }
 
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Modification([Bind(Include = "id,nationalite,email,phone,gsm,address,ville,dateNaiss")] Etudiant etudiant,string Update,String choix1,String choix2,String choix3)
+        {
+            ViewBag.Current = "Modification";
+
+            /*Update name of buttom if user click in Upload l image seule va etre modifie 
+             
+
+             */
+            if (ModelState.IsValid)
+            {
+                Etudiant etudiants = etudiantContext.etudiants.Find(etudiant.id);
+                
+                if (Request.Files.Count > 0 && Update=="Upload")
+                {
+                   //Recupere le fichier est le sauvegarder dans /image/
+                    HttpPostedFileBase file = Request.Files[0];
+                    string fileName = Path.GetFileNameWithoutExtension(file.FileName);
+                    string extension = Path.GetExtension(file.FileName);
+                    fileName = DateTime.Now.ToString("yymmssfff") + extension;
+                    etudiants.photo_link = fileName;
+                    fileName = Path.Combine(Server.MapPath("~/Image/"), fileName);
+                    file.SaveAs(fileName);
+
+                    etudiantContext.SaveChanges();
+                    return View(etudiants);
+                }
+                else
+                {
+                    //si clicke sur les valider les modification 
+                    etudiants.choix= choix1 + choix2 + choix3;
+                    etudiants.nationalite = etudiant.nationalite;
+                    etudiants.email = etudiant.email;
+                    etudiants.phone = etudiant.phone;
+                    etudiants.address = etudiant.address;
+                    etudiants.gsm = etudiant.gsm;
+                    etudiants.address = etudiant.address;
+                    etudiants.ville = etudiant.ville;
+                    etudiants.dateNaiss = etudiant.dateNaiss;
+
+                    etudiantContext.SaveChanges();
+                    return View(etudiants);
+                   // return RedirectToAction("Index");
+                }
+               
+            }
+            
+            return View(etudiant);
+        }
+
+        //****************************************************************************************************************************
+
+       
         public ActionResult Consulter()
         {
             ViewBag.Current = "Consulter";
@@ -32,5 +112,9 @@ namespace projetASP.Controllers
             ViewBag.Current = "Deconnecter";
             return View("Index");
         }
+
+
+  
+     
     }
 }
