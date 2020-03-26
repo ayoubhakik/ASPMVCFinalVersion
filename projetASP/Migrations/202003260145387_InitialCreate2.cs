1887eeda089c86bc1284f4aa0460f26421761005
Sifamo <sifane.mouad@gmail.com>
2020-03-26 04:02:10 +0100

    Finalizing register

---
+++ projetASP/Migrations/202003260145387_InitialCreate2.cs
@@ -0,0 +1,18 @@
+﻿namespace projetASP.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate2 : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Etudiants", "dateNaiss", c => c.DateTime());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Etudiants", "dateNaiss", c => c.DateTime(nullable: false));
+        }
+    }
+}
