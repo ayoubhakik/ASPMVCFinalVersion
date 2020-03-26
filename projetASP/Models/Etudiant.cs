9252cca838f8fd4dc9269e3edb6c7bf752fbaf61
Sifamo <sifane.mouad@gmail.com>
2020-03-26 04:28:38 +0100

    Finalizing register

--- projetASP/Models/Etudiant.cs
+++ projetASP/Models/Etudiant.cs
@@ -10,67 +10,65 @@ namespace projetASP.Models
     public class Etudiant
     {
         [Key]
-        [Required]
+        [Required(ErrorMessage = "Le CNE est obligatoire.")]
+        [StringLength(10, ErrorMessage = "Le CNE doit contenir 10 caractères. ")]
+        [RegularExpression("^[a-zA-Z0-9]*$", ErrorMessage = "Le CNE ne peut contenir que des lettres et des chiffres.")]
         public string cne { get; set; }
 
-
-
-
-        //[Required]
-
+        [Required(ErrorMessage = "Le nom est obligatoire.")]
+        [RegularExpression("^[a-zA-Z]*$", ErrorMessage = "Le nom ne peut contenir que des lettres.")]
         public string nom { get; set; }
-        //[Required]
 
+        [Required(ErrorMessage = "Le prenom est obligatoire.")]
+        [RegularExpression("^[a-zA-Z]*$", ErrorMessage = "Le prenom ne peut contenir que des lettres.")]
         public string prenom { get; set; }
-        //[Required]
+
+        [MinLength(6,ErrorMessage = "Le mot de pass doit dépasser six caractères.")]
         public string password { get; set; }
-        
-        //[Required]
+
+        [RegularExpression("^[a-zA-Z ]*$", ErrorMessage = "La nationalite ne peut contenir que des lettres.")]
         public string nationalite { get; set; }
-        //[Required]
+
+        [Required(ErrorMessage = "Le CIN est obligatoire.")]
+        [StringLength(8, ErrorMessage = "Le CNE doit contenir 8 caractères. ")]
+        [RegularExpression("^[a-zA-Z0-9]*$", ErrorMessage = "Le CIN ne peut contenir que des lettres et des chiffres.")]
         public string cin { get; set; }
 
-        
-
-
-       // [Required]
-
-
+        [EmailAddress(ErrorMessage = "Adresse email invalide.")]
         public string email { get; set; }
-        
-        //[Required]
+
+        [StringLength(10, ErrorMessage = "Le numéro de tel fixe doit contenir 10 chiffres")]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Le numéro de tel fixe ne peut contenir que des chiffres.")]
         public string phone { get; set; }
-        //[Required]
+
+        [StringLength(10, ErrorMessage = "Le numéro de tel portable doit contenir 10 chiffres")]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Le numéro tel portable ne peut contenir que des chiffres.")]
         public string gsm { get; set; }
-        //[Required]
+
+        [RegularExpression("^[a-zA-Z0-9 ]*$", ErrorMessage = "L'adresse ne peut contenir que des lettres et des chiffres.")]
         public string address { get; set; }
-        //[Required]
+
+        [RegularExpression("^[a-zA-Z]*$", ErrorMessage = "La ville ne peut contenir que des lettres.")]
         public string ville { get; set; }
         
-        //[Required]
         public string typeBac { get; set; }
-        //[Required]
-        public DateTime anneeBac { get; set; }
-        //[Required]
-     
 
+        public int anneeBac { get; set; }     
+
+        [Range(0,20, ErrorMessage = "La note de bac doit se situer entre 0 et 20.")]
         public double noteBac { get; set; }
-        //[Required]
+
         public string mentionBac { get; set; }
 
-        //[Required]
         public double noteFstYear { get; set; }
-        //[Required]
+
         public double noteSndYear { get; set; }
 
-        //[Required]
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
-        public DateTime dateNaiss { get; set; }
-        //[Required]
+        public Nullable<DateTime> dateNaiss { get; set; }
+
+        [RegularExpression("^[a-zA-Z]*$", ErrorMessage = "Le lieu de naissance ne peut contenir que des lettres.")]
         public string lieuNaiss { get; set; }
-
-        //[Required]
-
 
         public string photo_link { get; set; }
 
