0d2ee995ff0b9f0ef9944f793a0a5455afb01133
Sifamo <sifane.mouad@gmail.com>
2020-03-26 18:08:15 +0100

    tests

--- projetASP/Migrations/202003260044224_InitialCreate.Designer.cs
+++ projetASP/Migrations/202003260044224_InitialCreate.Designer.cs
@@ -7,13 +7,13 @@ namespace projetASP.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.4.0")]
-    public sealed partial class IntialCreate1 : IMigrationMetadata
+    public sealed partial class InitialCreate : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(IntialCreate1));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(InitialCreate));
         
         string IMigrationMetadata.Id
         {
-            get { return "202003241928044_IntialCreate1"; }
+            get { return "202003260044224_InitialCreate"; }
         }
         
         string IMigrationMetadata.Source
