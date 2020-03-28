6fe83ec78926dfa75f076f725fa7e3beb25cfff3
kamallabdo <Abderahmane.kamal@gmail.com>
2020-03-28 01:41:25 +0100

    sss

---
+++ projetASP/Migrations/202003272310268_IntialCreate1.cs
@@ -0,0 +1,18 @@
+﻿namespace projetASP.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class IntialCreate1 : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("dbo.Etudiants", "choix");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Etudiants", "choix", c => c.String());
+        }
+    }
+}
